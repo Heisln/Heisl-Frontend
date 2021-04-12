30a72b97c8432879f4b13c470bae10d2d84f530a
Simon Deutsch <simon.deutsch97@gmail.com>
2021-04-12 17:07:59 +0200

    booking cars works

--- frontend/src/app/pages/cars/car-book-modal/car-book-modal.module.ts
+++ frontend/src/app/pages/cars/car-book-modal/car-book-modal.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { CarBookModalPageRoutingModule } from './car-book-modal-routing.module';
-
 import { CarBookModalPage } from './car-book-modal.page';
+import { FormComponentModule } from 'src/app/reusables/form-components/form-component.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CarBookModalPageRoutingModule
+    CarBookModalPageRoutingModule,
+    ReactiveFormsModule,
+    FormComponentModule,
   ],
   declarations: [CarBookModalPage]
 })
--- frontend/src/app/redux/core.effects.ts
+++ frontend/src/app/redux/core.effects.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
@@ -20,9 +21,24 @@ export class CoreEffects {
   { dispatch: false }
   );
 
+  showToast$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(CoreActions.showToast),
+      map(async ({ toastText }) => {
+        const toast = await this.toastController.create({
+          message: toastText,
+          duration: 2000
+        });
+        void toast.present();
+      })
+    ),
+  { dispatch: false }
+  );
+
   constructor(
     private readonly store: Store,
     private actions$: Actions,
     private router: Router,
+    private toastController: ToastController,
   ) {}
 }
